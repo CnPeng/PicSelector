7bd574d71d221a8117db0080f1d191c885a06f77
CnPeng <893612134@qq.com>
2019-07-16 22:11:28 +0800

    新增已选择图片或网络图片的专用展示Gv

---
+++ piclib/src/main/java/com/cnpeng/piclib/tools/GlideUtil.kt
@@ -0,0 +1,53 @@
+package com.cnpeng.piclib.tools
+
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+import com.cnpeng.piclib.R
+
+/**
+ * CnPeng 2019-07-16
+ * 功用：
+ * 其他：
+ */
+object GlideUtil {
+
+    /**
+     * CnPeng 2019-05-10 08:34
+     * 功用：加载图片到指定 ImageView 中
+     * 说明：
+     * 1、兼容 Glide 的 load 方法所支持的： Url, drawableID ,bitmap ,drawable ,URI ,File ,URL ,ByteArray
+     * 2、由于 @BindingAdapter 中 requireAll=false 表示在布局文件中为 ImageView 引用这些属性时可以只传递部分内容
+     */
+    fun loadImage(iv: ImageView,
+                  imageUrl: String?,
+                  @DrawableRes drawableId: Int = 0,
+                  @DrawableRes defaultDrawableID: Int = R.drawable.ic_placeholder) {
+
+
+        val options = RequestOptions()
+                .error(defaultDrawableID)
+                .fitCenter()
+                .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+        //                .placeholder(lDefaultDrawableId)
+
+        Glide.with(iv)
+                .load(
+                        if (imageUrl.isNullOrEmpty()) {
+                            if (0 == drawableId) {
+                                defaultDrawableID
+                            } else {
+                                drawableId
+                            }
+                        } else {
+                            imageUrl
+                        }
+                )
+                .apply(options)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(iv)
+    }
+}
